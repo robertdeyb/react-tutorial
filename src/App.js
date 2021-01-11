b764dec572060a28cc4723d20ccdf382138af92b
Dave Torrente <Dave.Torrente@antser.com>
2021-01-11 08:31:38 +0000

    adding sample component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import './App.css';
-import { Component } from 'react';
-
+import React, { Component } from 'react';
+import Person from './Person/Person';
 class App extends Component {
   render() {
     return (
       <div className="App">
         <h1>Hi, I'm a React App</h1>
+        <Person/>
       </div>
     )
+    // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Sample Text'));
   }
 }
 
---
+++ src/Person/Person.js
@@ -0,0 +1,6 @@
+import React from 'react';
+const person = () => {
+    return <p>Hi</p>
+}
+
+export default person;
